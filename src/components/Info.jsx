f87882a4a3c93fc6a2741c6145c1d1ca6c1ba4e2
dpgaire <gairhedurga13@gmail.com>
2024-11-26 20:46:17 +0545

    update the graph ui

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -12,7 +12,7 @@ const Info = () => {
   return (
     <div className="p-4 border rounded-lg shadow-lg">
       <span className="font-bold">Points To Be Noted.</span>
-      <ul className="list-disc px-4 py-2">
+      <ul className="list-disc px-4 py-2 font-bold text-sm">
         {info.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
