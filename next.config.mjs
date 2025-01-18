19d7fad1d623168364d17e68c6aadcc3fcebc6cb
dpgaire <gairhedurga13@gmail.com>
2025-01-18 20:17:10 +0545

    create pwa app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public', // Where the service worker will be output
+  register: true, // Automatically registers the service worker
+  skipWaiting: true, // Activates the new service worker as soon as it's installed
+  // reactStrictMode: true, // Optional: Enable React's strict mode
+});
 
 export default nextConfig;
